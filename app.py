ff82ec16b757b62070aadc1f5788d54bab96f88e
michaelsandon <60781050+michaelsandon@users.noreply.github.com>
2023-07-18 05:07:02 +0000

    removed sensitive API key

--- app.py
+++ app.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template, jsonify, request
 #from database import get_cases_from_database
 from database import load_all_dv_cases, load_dv_case, add_report_to_db
 from flask_googlemaps import GoogleMaps, Map
+import os
 
 app = Flask(__name__)
-GoogleMaps(app, key='AIzaSyAGBFBFtRWx6jYIc_4BORyipbC37UTSATk')
+gmapapikey = os.environ['GOOGLEMAPS_API_KEY']
 
-mymap = Map(identifier="view-side",
-            lat=37.4419,
-            lng=-122.1419,
-            markers=[(37.4419, -122.1419)])
-
-#CASES = get_cases_from_database()
 Safehouseloactions = [{'lat': -31.8838752, 'lng': 115.8426928}]
 
 
@@ -35,7 +30,7 @@ def render_case_by_id(id):
 
 @app.route("/support")
 def render_support():
-  return render_template('support.html', mymap=mymap)
+  return render_template('support.html', gmapapikey=gmapapikey)
 
 
 @app.route("/support/incidentreported", methods=['POST'])
